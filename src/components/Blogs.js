c48dbe3a4b62d6f0d1ac75477702ebaac0494766
JRudransh <JRudransh@protonmail.com>
2022-02-01 17:29:15 +0530

    Working code

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,27 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from './scss/Blogs.module.scss'
 import Card from './Card';
 import axios from 'axios';
 
 const postUrl = 'https://jsonplaceholder.typicode.com/posts'
-const userUrl = 'https://jsonplaceholder.typicode.com/users'
+
 
 export default function Blogs() {
-    const [blog, setBlog] = useState({data: []})
-    axios.get(postUrl)
-    .then(res =>{
-        console.log({data: res.data});
-        console.log(blog);
-    })
+    const [blogs, setBlogs] = useState([])
+    useEffect(()=>{
+      axios.get(postUrl)
+      .then(res => {
+        let data = res.data
+        console.log(data);
+        setBlogs(data)
+      })
+    }, [])
+
+    // useEffect(fun)
+    // useEffect(fun, [])
+    // useEffect(fun, [n, x])
+
+    let blog_list = blogs.map((blog)=>(
+      <Card blog={blog} key={blog.id}/>
+    ))
   return(
       <div className={classes.container}>
           <div className={classes.grid}>
-            <Card class_name={classes.card}/>
-            <Card class_name={classes.card}/>
-            <Card class_name={classes.card}/>
-            <Card class_name={classes.card}/>
-            <Card class_name={classes.card}/>
-            <Card class_name={classes.card}/> 
+            {blog_list}
           </div>
       </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { CardContent, Card as Card_, CardHeader, Typography, CardActions } from '@mui/material';
+import { Button, Avatar } from '@mui/material';
+import axios from 'axios';
 
-export default function Card({class_name}) {
+let baseUrl = 'https://jsonplaceholder.typicode.com/users'
+
+export default function Card({blog}) {
+  // let name = `${user.firstName} ${user.lastName}`
+  let userId = blog.userId - 1
+  // let userUrl = `${baseUrl}/${userId}`
+  let name = 'Hello'
+  let [user, setUser] = useState([])
+  useEffect(()=>{
+    axios.get(baseUrl)
+    .then(res => {
+      setUser(res.data)
+      console.log(res.data);
+    })
+  }, [])
+  // axios.get(`${userUrl}/${blog.userId}`)
+  // .then(res=>{
+  //   console.log(res.data);
+  // })
+  // let currentUser = user[userId]
+  // let u_name = currentUser.name
+  let ref = `#${blog.id}`
   return(
-      <div className={class_name}>
-          <h1>I am heading</h1>
-          <h6>Sports</h6>
-          <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
-          <span>Rahul Das</span>
-      </div>
+      <Card_>
+          <CardHeader
+          avatar={
+            <Avatar alt={user.name}/>
+          }
+          title={user.name}
+          subheader="September 14, 2016"
+          />
+          <CardContent>
+            <Typography variant='h5'>{blog.title}</Typography>
+            <Typography variant='subtitle1'>Sports</Typography>
+            <Typography sx={{marginTop: '10px'}}>{blog.body}</Typography>
+          </CardContent>
+          <CardActions>
+            <Button href={ref}>Read More</Button>
+          </CardActions>
+      </Card_>
   );
 }
