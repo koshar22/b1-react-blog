6ec99a8a822101f79aefcb737839757815f56dee
koshar22 <saififiza454@gmail.com>
2022-02-03 21:39:12 +0530

    react-blog

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Grid } from '@mui/material';
 
 function App() {
   return (
     <div>
-      Hello
+      <Grid container spacing={1} columns={10}>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Grid container>
+            <Grid xs={5}>sub grid 1</Grid>
+            <Grid xs={5}>sub grid 2</Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} columns={8} sm={6} md={4} lg={3}>Item 10</Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>Item 1</Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>Item 1</Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>Item 1</Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>Item 1</Grid>
+      </Grid>
     </div>
   );
 }
